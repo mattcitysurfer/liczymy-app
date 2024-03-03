6d35b0f6ad77e0f271ef8fd0105044c43da860be
Mateusz Rodzoch <mateusz.rodzoch@pl.markant.com>
2024-03-03 16:26:35 +0100

    button for new numbers added + refactoring

--- src/app/counting/counting.component.ts
+++ src/app/counting/counting.component.ts
@@ -6,18 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./counting.component.css']
 })
 export class CountingComponent {
-  firstNumber: number | null = null;
-  secondNumber: number | null = null;
+  firstNumber: number = 0;
+  secondNumber: number = 0;
   result: number = 0;
-
   userResult: number | null = null;
   feedbackMessage: string = "";
 
   ngOnInit(): void {
-    let numbers: number[] = this.generateNumbers(100);
-    this.firstNumber = numbers[0];
-    this.secondNumber = numbers[1];
-    this.result = this.firstNumber + this.secondNumber;
+    this.generateNumbers(100);
   }
 
   checkResult() {
@@ -28,14 +24,17 @@ export class CountingComponent {
     }
   }
 
-  private generateNumbers(maxSum: number): [number, number] {
+  newNumbers() {
+    this.generateNumbers(100);
+  }
+
+  private generateNumbers(maxSum: number) {
     if (maxSum <= 1) {
       throw new Error('Maximum sum must be greater than 1 to get two positive numbers.');
     }
   
-    const firstNumber = Math.floor(Math.random() * (maxSum - 1)) + 1;
-    const secondNumber = Math.floor(Math.random() * (maxSum - firstNumber));
-  
-    return [firstNumber, secondNumber];
+    this.firstNumber = Math.floor(Math.random() * (maxSum - 1)) + 1;
+    this.secondNumber = Math.floor(Math.random() * (maxSum - this.firstNumber));
+    this.result = this.firstNumber + this.secondNumber;
   }
 }
